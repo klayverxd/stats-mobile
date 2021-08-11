5f8ad2994a050f7b8f66716ece4202f200da6b8a
klayverxd <klayverx@gmail.com>
2021-08-11 19:41:52 -0300

    add global theme

---
+++ src/global/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    background: '#FC4C02',
+    heading: '#DDE3F0',
+    primary: '#FC4C02',
+    secondary: '#E34602',
+    white: '#fff',
+    black: '#000',
+  },
+}
