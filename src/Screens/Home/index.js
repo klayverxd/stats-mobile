e48a336146d88b3425a5a453b4c6ae0bb3f95933
klayverxd <klayverx@gmail.com>
2021-08-13 23:54:38 -0300

    add content

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,19 +1,85 @@
 import React from 'react'
-import { StatusBar, Text, View } from 'react-native'
+import { Image, ScrollView, StatusBar, Text, View } from 'react-native'
 
 import { Header } from '../../components/Header'
+
+import Cyclist from '../../assets/cycling.png'
+import Cyclists from '../../assets/cyclists.png'
 
 import { styles } from './styles'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        // translucent
-      />
       <Header />
+
+      <ScrollView style={styles.containerCards}>
+        <View style={styles.containerCard}>
+          <View style={styles.cardSection1}>
+            <Text style={styles.title}>Distância total</Text>
+            <Text style={styles.subTitle}>9.607km</Text>
+          </View>
+          <View style={styles.cardSection2}>
+            <Text style={styles.title}>Elevação total</Text>
+            <Text style={styles.subTitle}>2.987m</Text>
+          </View>
+        </View>
+        <View style={styles.containerCard}>
+          <View style={styles.cardSection1}>
+            <Text style={styles.title}>Maior pedal</Text>
+            <Text style={styles.subTitle}>200km</Text>
+          </View>
+          <View style={styles.cardSection2}>
+            <Text style={styles.title}>Maior elevação</Text>
+            <Text style={styles.subTitle}>848m</Text>
+          </View>
+        </View>
+        <View style={styles.containerCard}>
+          <View style={styles.cardSection1}>
+            <Image source={Cyclist} style={styles.iconCard} />
+          </View>
+          <View style={styles.cardSection2}>
+            <Text style={styles.title}>Pedaladas</Text>
+            <Text style={styles.subTitle}>270</Text>
+          </View>
+        </View>
+        <View style={styles.containerCard}>
+          <View style={styles.cardSection1}>
+            <Image source={Cyclists} style={styles.iconCard} />
+          </View>
+          <View style={styles.cardSection2}>
+            <Text style={styles.title}>Clubes</Text>
+            <Text style={styles.subTitle}>19</Text>
+          </View>
+        </View>
+        <View style={styles.containerCardLastActivity}>
+          <Text style={styles.titleLastActivity}>Última atividade 🚲</Text>
+          <View style={styles.line}>
+            <Text style={styles.target}> Título:</Text>
+            <Text style={styles.description}>Angicos ☁️</Text>
+          </View>
+          <View style={styles.line}>
+            <Text style={styles.target}> Distância:</Text>
+            <Text style={styles.description}>49,3 km</Text>
+          </View>
+          <View style={styles.line}>
+            <Text style={styles.target}> Elevação:</Text>
+            <Text style={styles.description}>441 m</Text>
+          </View>
+          <View style={styles.line}>
+            <Text style={styles.target}> Kudos:</Text>
+            <Text style={styles.description}>23</Text>
+          </View>
+          <View style={styles.line}>
+            <Text style={styles.target}>Vel. média:</Text>
+            <Text style={styles.description}>18,3 km/h</Text>
+          </View>
+          <View style={styles.line}>
+            <Text style={styles.target}>Vel. máxima:</Text>
+            <Text style={styles.description}>36,4 km/h</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
--- src/Screens/Home/styles.js
+++ src/Screens/Home/styles.js
@@ -6,4 +6,80 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colors.primary,
   },
+  containerCards: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  containerCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    marginVertical: 12,
+    paddingVertical: 24,
+
+    borderWidth: 2,
+    borderColor: theme.colors.white,
+    borderRadius: 24,
+  },
+  title: {
+    color: theme.colors.white,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: theme.colors.white,
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  cardSection1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 2,
+    borderColor: theme.colors.white,
+  },
+  cardSection2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  containerCardLastActivity: {
+    marginVertical: 12,
+    paddingHorizontal: 20,
+    paddingVertical: 14,
+
+    borderWidth: 2,
+    borderColor: theme.colors.white,
+    borderRadius: 24,
+  },
+  titleLastActivity: {
+    color: theme.colors.white,
+
+    fontSize: 20,
+    fontWeight: 'bold',
+
+    marginBottom: 4,
+  },
+  line: {
+    flexDirection: 'row',
+
+    marginVertical: 4,
+    borderBottomWidth: 1,
+    borderColor: theme.colors.white,
+  },
+  target: {
+    color: theme.colors.white,
+    flex: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  description: {
+    color: theme.colors.white,
+    flex: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    justifyContent: 'flex-end',
+  },
 })
