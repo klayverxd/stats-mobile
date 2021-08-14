b79e5f8d2302aa1e9b8de6a4326e048068a5a725
klayverxd <klayverx@gmail.com>
2021-08-14 18:21:31 -0300

    add loading when load data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react'
 
+import { ActivityIndicator, View } from 'react-native'
+
 import AuthContext from '../contexts/auth'
 
 import AppRoutes from './app.routes'
 import AuthRoutes from './auth.routes'
 
 export default function Routes() {
-  const { signed } = useContext(AuthContext)
+  const { signed, loading } = useContext(AuthContext)
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#999" />
+      </View>
+    )
+  }
 
   return signed ? <AppRoutes /> : <AuthRoutes />
 }
