fa9910b1e6ba21ce5b852d96f26950f87efb6668
klayverxd <klayverx@gmail.com>
2021-08-14 17:36:44 -0300

    initial create activity screen

---
+++ src/Screens/CreateActivity/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import { Header } from '../../components/Header'
+
+import { styles } from './styles'
+
+export function CreateActivity() {
+  return (
+    <View style={styles.container}>
+      {/* <Header /> */}
+
+      <Text>CreateActivity Screen</Text>
+    </View>
+  )
+}
