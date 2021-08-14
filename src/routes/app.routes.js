272756a0ad2096c71e3413d4854cb33fe8857fd9
klayverxd <klayverx@gmail.com>
2021-08-14 19:07:12 -0300

    att routes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import { Image } from 'react-native'
+
 import { Header } from '../components/Header'
 
 import { Stats } from '../screens/Stats'
 import { CreateActivity } from '../screens/CreateActivity'
+import { Equipment } from '../screens/Equipment'
+
+import StatsImg from '../assets/logo-strava.png'
 
 const Tab = createBottomTabNavigator()
 
@@ -12,9 +17,24 @@ export default function App() {
   return (
     <>
       <Header />
-      <Tab.Navigator>
-        <Tab.Screen name="Stats" component={Stats} />
+      <Tab.Navigator
+        initialRouteName="Stats"
+        screenOptions={{
+          headerShown: false,
+        }}
+        // barStyle={{ backgroundColor: '#694fad' }}
+      >
+        <Tab.Screen
+          name="Stats"
+          component={Stats}
+          options={{
+            tabBarIcon: () => (
+              <Image source={StatsImg} style={{ width: 20, height: 20 }} />
+            ),
+          }}
+        />
         <Tab.Screen name="CreateActivity" component={CreateActivity} />
+        <Tab.Screen name="Equipment" component={Equipment} />
       </Tab.Navigator>
     </>
   )
