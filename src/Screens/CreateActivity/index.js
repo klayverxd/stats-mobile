0cc42129f56da76524bb280a4eb303fc297789e2
klayverxd <klayverx@gmail.com>
2021-08-15 12:55:20 -0300

    att create activity screen

--- src/Screens/CreateActivity/index.js
+++ src/Screens/CreateActivity/index.js
@@ -1,16 +1,83 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { forwardRef } from 'react'
 
-import { Header } from '../../components/Header'
+import { Image, Text, TextInput, View } from 'react-native'
+
+import CiclistIcon from '../../assets/ciclist-icon.png'
+import NoteIcon from '../../assets/note-icon.png'
+import RoadIcon from '../../assets/road-icon.png'
 
 import { styles } from './styles'
 
 export function CreateActivity() {
+  const [duration, setDuration] = useState({
+    hours: '1',
+    minutes: '15',
+    seconds: '30',
+  })
+
+  const [dataActivity, setDataActivity] = useState({
+    title: 'Atividade criada no Stats',
+    description:
+      'Descrição da atividade fica escrita aqui, este é um exemplo criado pelo Stats.',
+    type: 'Ride',
+    distance: 20000,
+    workout_type: 12,
+  })
+
+  const handleDataActivity = prop => event => {
+    setDataActivity({ ...dataActivity, [prop]: event.target.value })
+  }
+
+  const handleDuration = prop => event => {
+    setDuration({ ...duration, [prop]: event })
+  }
+
+  console.log(dataActivity.title)
+
   return (
     <View style={styles.container}>
-      {/* <Header /> */}
+      <View style={styles.containerInput}>
+        <Image source={CiclistIcon} style={styles.iconInput} />
+        <View style={styles.contentInput}>
+          <Text style={styles.labelInput}>Título da atividade: </Text>
+          <TextInput
+            style={styles.inputText}
+            onChange={handleDataActivity('title')}
+            value={dataActivity.title}
+            placeholder="digite o título da sua atividade..."
+          />
+        </View>
+      </View>
 
-      <Text>CreateActivity Screen</Text>
+      <View style={styles.containerInput}>
+        <Image source={NoteIcon} style={styles.iconInput} />
+        <View style={styles.contentInput}>
+          <Text style={styles.labelInput}>Descrição: </Text>
+          <TextInput
+            style={styles.inputText}
+            onChangeText={value => handleDataActivity('description', value)}
+            defaultValue={dataActivity.description}
+            placeholder="digite a descrição da sua atividade..."
+            multiline
+            numberOfLines={5}
+          />
+        </View>
+      </View>
+
+      <View style={styles.containerInput}>
+        <Image source={RoadIcon} style={styles.iconInput} />
+        <View style={styles.contentInputRow}>
+          <Text style={styles.labelInput}>Distância: </Text>
+          <TextInput
+            style={styles.inputText}
+            onChange={value => handleDataActivity(value)}
+            value={dataActivity.distance}
+            keyboardType="numeric"
+            // placeholder="digite a distância da sua atividade..."
+          />
+        </View>
+      </View>
     </View>
   )
 }
--- src/Screens/CreateActivity/styles.js
+++ src/Screens/CreateActivity/styles.js
@@ -3,83 +3,69 @@ import { theme } from '../../global/styles/theme'
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: theme.colors.primary,
-  },
-  containerCards: {
+
     flex: 1,
+
     paddingHorizontal: 16,
     paddingVertical: 12,
   },
-  containerCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
 
-    marginVertical: 12,
-    paddingVertical: 24,
+  containerInput: {
+    alignItems: 'center',
 
     borderWidth: 2,
     borderColor: theme.colors.white,
     borderRadius: 24,
+
+    flexDirection: 'row',
+
+    minHeight: 64,
+    marginVertical: 12,
+
+    padding: 10,
   },
-  title: {
+
+  iconInput: {
+    width: 32,
+    height: 32,
+
+    marginRight: 12,
+  },
+
+  contentInput: {
+    flex: 1,
+    borderLeftWidth: 1,
+    borderColor: theme.colors.white,
+
+    paddingLeft: 12,
+    minHeight: 64,
+  },
+
+  contentInputRow: {
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderColor: theme.colors.white,
+
+    flex: 1,
+    flexDirection: 'row',
+
+    paddingLeft: 12,
+    minHeight: 64,
+  },
+
+  labelInput: {
     color: theme.colors.white,
     fontSize: 18,
-    fontWeight: 'bold',
-  },
-  subTitle: {
-    color: theme.colors.white,
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  cardSection1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRightWidth: 2,
-    borderColor: theme.colors.white,
-  },
-  cardSection2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  containerCardLastActivity: {
-    marginVertical: 12,
-    paddingHorizontal: 20,
-    paddingVertical: 14,
-
-    borderWidth: 2,
-    borderColor: theme.colors.white,
-    borderRadius: 24,
-  },
-  titleLastActivity: {
-    color: theme.colors.white,
-
-    fontSize: 20,
     fontWeight: 'bold',
 
     marginBottom: 4,
   },
-  line: {
-    flexDirection: 'row',
 
-    marginVertical: 4,
-    borderBottomWidth: 1,
-    borderColor: theme.colors.white,
-  },
-  target: {
+  inputText: {
     color: theme.colors.white,
-    flex: 1,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  description: {
-    color: theme.colors.white,
-    flex: 1,
-    fontSize: 18,
-    fontWeight: 'bold',
-    justifyContent: 'flex-end',
+
+    width: '100%',
+    fontSize: 16,
   },
 })
