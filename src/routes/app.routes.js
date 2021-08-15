84867562ff053b3b6f07786e36c89632ed2f65ed
klayverxd <klayverx@gmail.com>
2021-08-15 11:12:47 -0300

    tab navigation styles

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { Image } from 'react-native'
+import { Image, StatusBar } from 'react-native'
 
 import { Header } from '../components/Header'
 
@@ -9,32 +9,60 @@ import { Stats } from '../screens/Stats'
 import { CreateActivity } from '../screens/CreateActivity'
 import { Equipment } from '../screens/Equipment'
 
-import StatsImg from '../assets/logo-strava.png'
+import BarChart from '../assets/bar-chart.png'
+import Road from '../assets/road.png'
+import Wheel from '../assets/wheel.png'
+
+import { theme } from '../global/styles/theme'
 
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <>
+      <StatusBar barStyle="dark-content" />
       <Header />
       <Tab.Navigator
         initialRouteName="Stats"
         screenOptions={{
           headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#252525',
+          },
+          tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.5)',
+          tabBarActiveTintColor: theme.colors.primary,
         }}
-        // barStyle={{ backgroundColor: '#694fad' }}
       >
         <Tab.Screen
           name="Stats"
           component={Stats}
           options={{
+            title: 'Estatísticas',
             tabBarIcon: () => (
-              <Image source={StatsImg} style={{ width: 20, height: 20 }} />
+              <Image source={BarChart} style={{ width: 20, height: 20 }} />
             ),
           }}
         />
-        <Tab.Screen name="CreateActivity" component={CreateActivity} />
-        <Tab.Screen name="Equipment" component={Equipment} />
+        <Tab.Screen
+          name="CreateActivity"
+          component={CreateActivity}
+          options={{
+            title: 'Criar atividade',
+            tabBarIcon: () => (
+              <Image source={Road} style={{ width: 20, height: 20 }} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Equipment"
+          component={Equipment}
+          options={{
+            title: 'Equipamentos',
+            tabBarIcon: () => (
+              <Image source={Wheel} style={{ width: 20, height: 20 }} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </>
   )
